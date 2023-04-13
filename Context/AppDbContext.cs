0792fb824a5e81693381561c35abb74c02f91007
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-12 21:11:12 -0300

    Criando contexto, serviço para contexto e connection string

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VendaLanches.Models;
+
+namespace VendaLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        private DbSet<Lanche> lanches;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
