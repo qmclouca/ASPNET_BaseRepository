7afab1481b798413d81a8c900c364d3802cecc69
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-12 22:09:13 -0300

    Data Annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace VendaLanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendaLanches.Models
 {
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [MinLength(3, ErrorMessage = "O nome da categoria deve ter no mínimo {1} caracteres.")]
+        [MaxLength(200, ErrorMessage = "O nome da categoria deve ter no máximo {1} caracteres.")]
+        [StringLength(200)]
         public string CategoriaNome { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [MinLength(3, ErrorMessage = "A descrição da categoria deve ter no mínimo {1} caracteres.")]
+        [MaxLength(200, ErrorMessage = "A descrição da categoria deve ter no máximo {1} caracteres.")]
+        [StringLength(200)]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -1,9 +1,20 @@
-﻿namespace VendaLanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VendaLanches.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+        
+        [Required(ErrorMessage = "Informe o nome do lanche")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição curta do lanche")]
+        [MinLength(20, ErrorMessage = "A descrição de deve ter no mínimo {1} caracteres.")]
+        [MaxLength(200, ErrorMessage = "A descrição de deve ter no máximo {1} caracteres.")]
         public string DescricaoCurta { get; set; }
         public string DescricaoDetalhada { get; set; }
         public decimal Preco { get; set; }
@@ -14,5 +25,8 @@
 
         public int CategoriaId { get; set; }
         public virtual Categoria Categoria { get; set; }
+
+        [NotMapped]
+        public DateTime DataCriacao { get; set; }
     }
 }
