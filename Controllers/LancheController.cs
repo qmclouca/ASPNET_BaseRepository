e480b29d5be54a39b46aed8afe3dfbb0c6ad0d68
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-16 09:45:50 -0300

    Criando View para Lista de lanches

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendaLanches.Models;
+using VendaLanches.Repositories.Interfaces;
+
+namespace VendaLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            IEnumerable<Lanche> lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
