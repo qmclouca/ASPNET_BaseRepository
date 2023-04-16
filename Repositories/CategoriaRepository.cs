290a53707657af0368e0d952b0261143f8a5a57a
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-15 21:57:59 -0300

    criando contexto para categorias

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using VendaLanches.Context;
+using VendaLanches.Models;
+using VendaLanches.Repositories.Interfaces;
+
+namespace VendaLanches.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public CategoriaRepository(AppDbContext context)
+        {
+            _appDbContext = context;
+        }
+        public IEnumerable<Categoria> Categorias => throw new NotImplementedException();
+    }
+}
