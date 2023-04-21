d97f1e5bdb66a7e52f3670ea795a9935795f51fa
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-21 19:12:35 -0300

    AdicionarAoCarrinho

--- Models/CarrinhoCompra.cs
+++ Models/CarrinhoCompra.cs
@@ -21,5 +21,26 @@ namespace VendaLanches.Models
             session.SetString("CarrinhoId", carrinhoId);
             return new CarrinhoCompra(context) { CarrinhoCompraId = carrinhoId };
         }
+
+        public void AdicionarAoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                               s => s.Lanche.LancheId == lanche.LancheId && s.CarrinhoCompraId == CarrinhoCompraId);
+            if (carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+        }
     }
 }
