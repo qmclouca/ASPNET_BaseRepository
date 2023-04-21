9593200d7d1d6bd964cabafd98906d0136ad1ab1
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-21 18:43:03 -0300

    Criando entidade CarrinhoCompraItem

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -12,5 +12,6 @@ namespace VendaLanches.Context
         }
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
     }
 }
