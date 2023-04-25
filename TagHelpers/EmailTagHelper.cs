fecf030af2c5bb606351fb01e469e7b07ef2ebfd
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-04-25 18:35:37 -0300

    TagHelpers

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VendaLanches.TagHelpers
+{
+    public class EmailTagHelper: TagHelper
+    {
+        public string Endereco { get; set; }
+        public string Conteudo { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; // Replaces <email> with <a> tag
+            output.Attributes.SetAttribute("href", "mailto:" + Endereco);
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
