e99a93e70f08111943470048ab9ed175d449e644
qmclouca <qmclouca@gmail.com>
2023-04-28 19:02:47 -0300

    Dropdown categorias de lanches

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendaLanches.Repositories.Interfaces;
+
+namespace VendaLanches.Components
+{
+    public class CategoriaMenu: ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categoria = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categoria);
+        }
+    }
+}
