c6bc092a698dab7e9bce5bcee5ffabfb357da002
qmclouca <qmclouca@gmail.com>
2023-05-04 08:44:27 -0300

    Formulário para filtrar por produto

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -31,7 +31,7 @@ namespace VendaLanches.Controllers
                 lanches = _lancheRepository.Lanches
                     .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
                     .OrderBy(c => c.Nome);
-                
+
                 categoriaAtual = categoria;
             }
 
@@ -48,6 +48,32 @@ namespace VendaLanches.Controllers
         {
             var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId.Equals(id));
             return View(lanche);
+        }
+
+        public IActionResult Search(string searchString)
+        {
+            
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                lanches = _lancheRepository.Lanches
+                    .Where(l => l.Nome.ToLower().Contains(searchString.ToLower()));
+                if (lanches.Any()) categoriaAtual = "Lanches";
+                else categoriaAtual = "Nenhum lanche foi encontrado";
+            }
+
+            return View("~/Views/Lanche/List.cshtml", new LancheListViewModel 
+            { 
+                Lanches = lanches, 
+                CategoriaAtual = "Todos os lanches" 
+            });
         }
     }
 }
