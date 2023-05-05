ee08def82f4b720fb6b8422a9f3bc6f778a236df
qmclouca <qmclouca@gmail.com>
2023-05-05 08:45:36 -0300

    CheckOut

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendaLanches.Models;
+using VendaLanches.Repositories.Interfaces;
+
+namespace VendaLanches.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public PedidoController(CarrinhoCompra carrinhoCompra, IPedidoRepository pedidoRepository)
+        {
+            _carrinhoCompra = carrinhoCompra;
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            var items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = items;
+
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, inclua um lanche...");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+                _carrinhoCompra.LimparCarrinho();
+                return RedirectToAction("CheckoutCompleto");
+            }
+
+            return View(pedido);
+        }
+    }
+}
