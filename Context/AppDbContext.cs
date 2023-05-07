79fdbade63b8b3bcc641bcdca4f3929554b77089
qmclouca <qmclouca@gmail.com>
2023-05-07 09:39:16 -0300

    Configurações dos pacotes do Identity, inclusão dos serviços no startup e configuração

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using VendaLanches.Models;
 
 namespace VendaLanches.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         private DbSet<Lanche> lanches;
 
